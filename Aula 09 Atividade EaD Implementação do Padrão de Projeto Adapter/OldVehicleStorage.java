d32dfb4971ede7c1cc836b69fe0a08eaffd7049d
Samuel Toledo <102129312+TIOSAMBR@users.noreply.github.com>
2024-08-02 21:43:43 -0300

    Aula 09 Atividade EaD Implementação do Padrão de Projeto Adapter
    Aula 09 Atividade EaD Implementação do Padrão de Projeto Adapter

---
+++ Aula 09 Atividade EaD Implementação do Padrão de Projeto Adapter/OldVehicleStorage.java
@@ -0,0 +1,6 @@
+public class OldVehicleStorage {
+    public void storeVehicleData(String data) {
+        // Lógica para armazenar dados no formato específico da biblioteca legada
+        System.out.println("Dados armazenados na biblioteca legada: " + data);
+    }
+}
